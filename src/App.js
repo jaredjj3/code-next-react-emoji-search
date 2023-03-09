a7e9d468616d55d99d39301024bec7dba3e31409
jaredjj3 <jared@jaredjohnson.dev>
2023-03-09 05:38:21 -0500

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './Header';
 import { Input } from './Input';
 import { Results } from './Results';
 import ALL_EMOJIS from './data/ALL_EMOJIS.json';
 
-const SEARCH_LIMIT = 20;
-const INITIAL_EMOJIS = ALL_EMOJIS.slice(0, SEARCH_LIMIT);
-
 export default function App() {
-  const [emojis, setEmojis] = useState(INITIAL_EMOJIS);
+  const [emojis, setEmojis] = useState(ALL_EMOJIS);
+  const [query, setQuery] = useState('');
 
   return (
     <div class="container">
--- src/data/search.js
+++
@@ -1,5 +0,0 @@
-import ALL_EMOJIS from './ALL_EMOJIS.json';
-
-export const search = (query, limit) => {
-  return ALL_EMOJIS.slice(0, limit);
-};
